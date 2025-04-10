b7b100151e0924b69a67bff909501fd37311e93c
Diwakar G <73572033+diwakarworks@users.noreply.github.com>
2025-04-10 09:35:58 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,6 @@ import { FaGithub, FaXTwitter, FaLinkedin} from "react-icons/fa6";
 
 import { SiLeetcode } from "react-icons/si";
 
-import {FileDown} from "lucide-react";
-
 const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
@@ -31,13 +29,6 @@ const Footer = () => {
         rel="noopener noreferrer"
       >
         <SiLeetcode size={24} />
-      </a>
-      <a
-        href="https://drive.google.com/file/d/1QiNynmsfdTn4jH5TW58tmhao3ig04oIH/view"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      <FileDown className="w-6 h-6 mr-4" />
       </a>
     </footer>
   );
